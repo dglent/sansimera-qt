968b851debc35dc3e37c22e9ac06108497643b77
Dimitrios Glentadakis <dglent@free.fr>
2026-04-25 06:35:44 +0200

    Bump version to 1.2.0

--- sansimera_qt/sansimera_qt.py
+++ sansimera_qt/sansimera_qt.py
@@ -31,7 +31,7 @@ except ImportError:
     from sansimera_qt import sansimera_fetch
     from sansimera_qt import sansimera_reminder
 
-__version__ = "1.1.1"
+__version__ = "1.2.0"
 
 
 class Sansimera(QMainWindow):
